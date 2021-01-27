f2c0d4a01a9ea8eafc18cf417ab83383e217fa2f
Max Whale <maxwhale99@gmail.com>
2021-01-27 15:55:31 +0000

    Restore grey counter

--- picorv32/example.v
+++ picorv32/example.v
@@ -8,8 +8,8 @@ module top (
 );
 	reg [7:0] leds1;
 	reg [7:0] leds2;
-	reg [7:0] leds3;
-	reg [7:0] leds4;
+	reg [7:0] leds3 = 8'b0;
+	reg [7:0] leds4 = 8'b0;
 
 	reg [2:0] brightness = 3'b111;
 
@@ -59,10 +59,10 @@ module top (
 	reg [31:0] mem_rdata;
 
 	picorv32 #(
-		.ENABLE_COUNTERS(0),
+		.ENABLE_COUNTERS(1),
 		.LATCHED_MEM_RDATA(1),
 		.TWO_STAGE_SHIFT(0),
-		.TWO_CYCLE_ALU(1),
+		.TWO_CYCLE_ALU(0),
 		.CATCH_MISALIGN(0),
 		.CATCH_ILLINSN(0)
 	) cpu (
@@ -88,10 +88,10 @@ module top (
 	reg [31:0] mem_rdata2;
 
 	picorv32 #(
-		.ENABLE_COUNTERS(0),
+		.ENABLE_COUNTERS(1),
 		.LATCHED_MEM_RDATA(1),
 		.TWO_STAGE_SHIFT(0),
-		.TWO_CYCLE_ALU(1),
+		.TWO_CYCLE_ALU(0),
 		.CATCH_MISALIGN(0),
 		.CATCH_ILLINSN(0)
 	) cpu2 (
@@ -105,63 +105,63 @@ module top (
 		.mem_rdata(mem_rdata2)
 	);
 
-	// -------------------------------
-	// PicoRV32 Core3
+	// // -------------------------------
+	// // PicoRV32 Core3
 
-	wire mem_valid3;
-	wire [31:0] mem_addr3;
-	wire [31:0] mem_wdata3;
-	wire [3:0] mem_wstrb3;
+	// wire mem_valid3;
+	// wire [31:0] mem_addr3;
+	// wire [31:0] mem_wdata3;
+	// wire [3:0] mem_wstrb3;
 
-	reg mem_ready3;
-	reg [31:0] mem_rdata3;
+	// reg mem_ready3;
+	// reg [31:0] mem_rdata3;
 
-	picorv32 #(
-		.ENABLE_COUNTERS(0),
-		.LATCHED_MEM_RDATA(1),
-		.TWO_STAGE_SHIFT(0),
-		.TWO_CYCLE_ALU(1),
-		.CATCH_MISALIGN(0),
-		.CATCH_ILLINSN(0)
-	) cpu3 (
-		.clk      (clk      ),
-		.resetn   (resetn   ),
-		.mem_valid(mem_valid3),
-		.mem_ready(mem_ready3),
-		.mem_addr (mem_addr3 ),
-		.mem_wdata(mem_wdata3),
-		.mem_wstrb(mem_wstrb3),
-		.mem_rdata(mem_rdata3)
-	);
+	// picorv32 #(
+	// 	.ENABLE_COUNTERS(0),
+	// 	.LATCHED_MEM_RDATA(1),
+	// 	.TWO_STAGE_SHIFT(0),
+	// 	.TWO_CYCLE_ALU(1),
+	// 	.CATCH_MISALIGN(0),
+	// 	.CATCH_ILLINSN(0)
+	// ) cpu3 (
+	// 	.clk      (clk      ),
+	// 	.resetn   (resetn   ),
+	// 	.mem_valid(mem_valid3),
+	// 	.mem_ready(mem_ready3),
+	// 	.mem_addr (mem_addr3 ),
+	// 	.mem_wdata(mem_wdata3),
+	// 	.mem_wstrb(mem_wstrb3),
+	// 	.mem_rdata(mem_rdata3)
+	// );
 
-	// -------------------------------
-	// PicoRV32 Core4
+	// // -------------------------------
+	// // PicoRV32 Core4
 
-	wire mem_valid4;
-	wire [31:0] mem_addr4;
-	wire [31:0] mem_wdata4;
-	wire [3:0] mem_wstrb4;
+	// wire mem_valid4;
+	// wire [31:0] mem_addr4;
+	// wire [31:0] mem_wdata4;
+	// wire [3:0] mem_wstrb4;
 
-	reg mem_ready4;
-	reg [31:0] mem_rdata4;
+	// reg mem_ready4;
+	// reg [31:0] mem_rdata4;
 
-	picorv32 #(
-		.ENABLE_COUNTERS(0),
-		.LATCHED_MEM_RDATA(1),
-		.TWO_STAGE_SHIFT(0),
-		.TWO_CYCLE_ALU(1),
-		.CATCH_MISALIGN(0),
-		.CATCH_ILLINSN(0)
-	) cpu4 (
-		.clk      (clk      ),
-		.resetn   (resetn   ),
-		.mem_valid(mem_valid4),
-		.mem_ready(mem_ready4),
-		.mem_addr (mem_addr4 ),
-		.mem_wdata(mem_wdata4),
-		.mem_wstrb(mem_wstrb4),
-		.mem_rdata(mem_rdata4)
-	);
+	// picorv32 #(
+	// 	.ENABLE_COUNTERS(0),
+	// 	.LATCHED_MEM_RDATA(1),
+	// 	.TWO_STAGE_SHIFT(0),
+	// 	.TWO_CYCLE_ALU(1),
+	// 	.CATCH_MISALIGN(0),
+	// 	.CATCH_ILLINSN(0)
+	// ) cpu4 (
+	// 	.clk      (clk      ),
+	// 	.resetn   (resetn   ),
+	// 	.mem_valid(mem_valid4),
+	// 	.mem_ready(mem_ready4),
+	// 	.mem_addr (mem_addr4 ),
+	// 	.mem_wdata(mem_wdata4),
+	// 	.mem_wstrb(mem_wstrb4),
+	// 	.mem_rdata(mem_rdata4)
+	// );
 
 
 	// -------------------------------
@@ -175,11 +175,11 @@ module top (
 	reg [31:0] memory2 [0:MEM_SIZE-1];
 	initial $readmemh("firmware.hex", memory2);
 
-	reg [31:0] memory3 [0:MEM_SIZE-1];
-	initial $readmemh("firmware.hex", memory3);
+	// reg [31:0] memory3 [0:MEM_SIZE-1];
+	// initial $readmemh("firmware.hex", memory3);
 
-	reg [31:0] memory4 [0:MEM_SIZE-1];
-	initial $readmemh("firmware.hex", memory4);
+	// reg [31:0] memory4 [0:MEM_SIZE-1];
+	// initial $readmemh("firmware.hex", memory4);
 
 	always @(posedge clk) begin
 		mem_ready <= 0;
@@ -226,45 +226,45 @@ module top (
 				end
 			endcase
 		end
-		if (resetn && mem_valid3 && !mem_ready3) begin
-			(* parallel_case *)
-			case (1)
-				!mem_wstrb3 && (mem_addr3 >> 2) < MEM_SIZE: begin
-					mem_rdata3 <= memory3[mem_addr3 >> 2];
-					mem_ready3 <= 1;
-				end
-				|mem_wstrb3 && (mem_addr3 >> 2) < MEM_SIZE: begin
-					if (mem_wstrb3[0]) memory3[mem_addr3 >> 2][ 7: 0] <= mem_wdata3[ 7: 0];
-					if (mem_wstrb3[1]) memory3[mem_addr3 >> 2][15: 8] <= mem_wdata3[15: 8];
-					if (mem_wstrb3[2]) memory3[mem_addr3 >> 2][23:16] <= mem_wdata3[23:16];
-					if (mem_wstrb3[3]) memory3[mem_addr3 >> 2][31:24] <= mem_wdata3[31:24];
-					mem_ready3 <= 1;
-				end
-				|mem_wstrb3 && mem_addr3 == 32'h1000_0000: begin
-					leds3 <= mem_wdata3;
-					mem_ready3 <= 1;
-				end
-			endcase
-		end
-		if (resetn && mem_valid4 && !mem_ready4) begin
-			(* parallel_case *)
-			case (1)
-				!mem_wstrb4 && (mem_addr4 >> 2) < MEM_SIZE: begin
-					mem_rdata4 <= memory4[mem_addr4 >> 2];
-					mem_ready4 <= 1;
-				end
-				|mem_wstrb4 && (mem_addr4 >> 2) < MEM_SIZE: begin
-					if (mem_wstrb4[0]) memory4[mem_addr4 >> 2][ 7: 0] <= mem_wdata4[ 7: 0];
-					if (mem_wstrb4[1]) memory4[mem_addr4 >> 2][15: 8] <= mem_wdata4[15: 8];
-					if (mem_wstrb4[2]) memory4[mem_addr4 >> 2][23:16] <= mem_wdata4[23:16];
-					if (mem_wstrb4[3]) memory4[mem_addr4 >> 2][31:24] <= mem_wdata4[31:24];
-					mem_ready4 <= 1;
-				end
-				|mem_wstrb4 && mem_addr4 == 32'h1000_0000: begin
-					leds4 <= mem_wdata4;
-					mem_ready4 <= 1;
-				end
-			endcase
-		end
+		// if (resetn && mem_valid3 && !mem_ready3) begin
+		// 	(* parallel_case *)
+		// 	case (1)
+		// 		!mem_wstrb3 && (mem_addr3 >> 2) < MEM_SIZE: begin
+		// 			mem_rdata3 <= memory3[mem_addr3 >> 2];
+		// 			mem_ready3 <= 1;
+		// 		end
+		// 		|mem_wstrb3 && (mem_addr3 >> 2) < MEM_SIZE: begin
+		// 			if (mem_wstrb3[0]) memory3[mem_addr3 >> 2][ 7: 0] <= mem_wdata3[ 7: 0];
+		// 			if (mem_wstrb3[1]) memory3[mem_addr3 >> 2][15: 8] <= mem_wdata3[15: 8];
+		// 			if (mem_wstrb3[2]) memory3[mem_addr3 >> 2][23:16] <= mem_wdata3[23:16];
+		// 			if (mem_wstrb3[3]) memory3[mem_addr3 >> 2][31:24] <= mem_wdata3[31:24];
+		// 			mem_ready3 <= 1;
+		// 		end
+		// 		|mem_wstrb3 && mem_addr3 == 32'h1000_0000: begin
+		// 			leds3 <= mem_wdata3;
+		// 			mem_ready3 <= 1;
+		// 		end
+		// 	endcase
+		// end
+		// if (resetn && mem_valid4 && !mem_ready4) begin
+		// 	(* parallel_case *)
+		// 	case (1)
+		// 		!mem_wstrb4 && (mem_addr4 >> 2) < MEM_SIZE: begin
+		// 			mem_rdata4 <= memory4[mem_addr4 >> 2];
+		// 			mem_ready4 <= 1;
+		// 		end
+		// 		|mem_wstrb4 && (mem_addr4 >> 2) < MEM_SIZE: begin
+		// 			if (mem_wstrb4[0]) memory4[mem_addr4 >> 2][ 7: 0] <= mem_wdata4[ 7: 0];
+		// 			if (mem_wstrb4[1]) memory4[mem_addr4 >> 2][15: 8] <= mem_wdata4[15: 8];
+		// 			if (mem_wstrb4[2]) memory4[mem_addr4 >> 2][23:16] <= mem_wdata4[23:16];
+		// 			if (mem_wstrb4[3]) memory4[mem_addr4 >> 2][31:24] <= mem_wdata4[31:24];
+		// 			mem_ready4 <= 1;
+		// 		end
+		// 		|mem_wstrb4 && mem_addr4 == 32'h1000_0000: begin
+		// 			leds4 <= mem_wdata4;
+		// 			mem_ready4 <= 1;
+		// 		end
+		// 	endcase
+		// end
 	end
 endmodule
