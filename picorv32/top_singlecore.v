f56e8ce5e9f5a99be12d78cf4c8e948cc47bbf7c
Max Whale <maxwhale99@gmail.com>
2021-01-31 16:20:27 +0000

    More efficient memory address decoding

--- picorv32/top_singlecore.v
+++ picorv32/top_singlecore.v
@@ -46,6 +46,15 @@ module top (
 	);
 
 	// -------------------------------
+	// Memory/IO Interface
+
+	// 2048 32bit words = 8192 bytes memory
+	localparam MEM_SIZE = 2048;
+	localparam MEM_BITS = $clog2(MEM_SIZE);
+	reg [31:0] memory [0:MEM_SIZE-1];
+	initial $readmemh("firmware.hex", memory);
+
+	// -------------------------------
 	// PicoRV32 Core
 
 	wire [31:0] mem_la_addr;
@@ -54,7 +63,8 @@ module top (
 	wire        mem_la_read;
 	wire        mem_la_write;
 
-	reg [31:0] mem_addr;
+	reg [3:0] mem_addr_high;
+	reg [MEM_BITS-1:0] mem_addr_low;
 	reg [31:0] mem_wdata;
 	reg [ 3:0] mem_wstrb;
 	reg        mem_read = 0;
@@ -101,18 +111,11 @@ module top (
 		.ready   (tx_ready)
 	);
 
-	// -------------------------------
-	// Memory/IO Interface
-
-	// 2048 32bit words = 8192 bytes memory
-	localparam MEM_SIZE = 2048;
-	reg [31:0] memory [0:MEM_SIZE-1];
-	initial $readmemh("firmware.hex", memory);
-
 	always @(posedge clk) begin
 
 		if (mem_la_read || mem_la_write) begin
-			mem_addr  <= mem_la_addr;
+			mem_addr_low <= mem_la_addr[MEM_BITS+1:2];
+			mem_addr_high <= mem_la_addr[31:28];
 			mem_wdata <= mem_la_wdata;
 			mem_read  <= mem_la_read;
 			mem_write <= mem_la_write;
@@ -126,26 +129,26 @@ module top (
 		if (resetn && (mem_read || mem_write) && !mem_ready) begin
 			(* parallel_case *)
 			case (1)
-				mem_read && (mem_addr >> 2) < MEM_SIZE: begin
-					mem_rdata <= memory[mem_addr >> 2];
+				mem_read && mem_addr_high == 4'h0: begin
+					mem_rdata <= memory[mem_addr_low];
 					mem_ready <= 1;
 				end
-				mem_write && (mem_addr >> 2) < MEM_SIZE: begin
-					if (mem_wstrb[0]) memory[mem_addr >> 2][ 7: 0] <= mem_wdata[ 7: 0];
-					if (mem_wstrb[1]) memory[mem_addr >> 2][15: 8] <= mem_wdata[15: 8];
-					if (mem_wstrb[2]) memory[mem_addr >> 2][23:16] <= mem_wdata[23:16];
-					if (mem_wstrb[3]) memory[mem_addr >> 2][31:24] <= mem_wdata[31:24];
+				mem_write && mem_addr_high == 4'h0: begin
+					if (mem_wstrb[0]) memory[mem_addr_low][ 7: 0] <= mem_wdata[ 7: 0];
+					if (mem_wstrb[1]) memory[mem_addr_low][15: 8] <= mem_wdata[15: 8];
+					if (mem_wstrb[2]) memory[mem_addr_low][23:16] <= mem_wdata[23:16];
+					if (mem_wstrb[3]) memory[mem_addr_low][31:24] <= mem_wdata[31:24];
 					mem_ready <= 1;
 				end
-				mem_write && mem_addr == 32'h1000_0000: begin
+				mem_write && mem_addr_high == 4'h1: begin
 					leds[7:0] <= mem_wdata[7:0];
 					mem_ready <= 1;
 				end
-                mem_read && mem_addr == 32'h2000_0000: begin
+                mem_read && mem_addr_high == 4'h2: begin
 					mem_rdata <= {31'b0, tx_ready};
 					mem_ready <= 1;
 				end
-                mem_write && mem_addr == 32'h2000_0000: begin
+                mem_write && mem_addr_high == 4'h2: begin
 					tx_data   <= mem_wdata[7:0];
 					tx_send   <= 1;
 					mem_ready <= 1;
